3037293365d9bf75252b1e4915d907b2d294f379
Es1amMohamed <EslamMohamedMetwally1@gmail.com>
2022-05-03 13:40:30 +0200

    first commit

---
+++ Components.md
@@ -0,0 +1,18 @@
+Note :
+    -Owner
+    -Title
+    -Content
+    -Created_at
+    -Slug
+
+
+User:
+       -Ability To :
+        -Login
+        -Create a Profile
+        -Create a Note
+        -Post a Note
+
+Apps :
+    -Note 
+    -accounts
---
+++ notes/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+'''
+To do :
+    -Owner
+    -Title
+    -content
+    -Created_at
+    -slug
+'''
+class Note(models.Model):
+    owner = models.ForeignKey(User,related_name='owner',on_delete=models.CASCADE)
+    title = models.CharField(max_length=100,unique=True)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+    slug = models.SlugField(unique=True, null=True , blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Note, self).save(*args, **kwargs)
